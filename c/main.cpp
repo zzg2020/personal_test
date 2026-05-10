a4a1229a12f293c5ca472fcc29b32834ab6fce21
zzg <2978644367@qq.com>
2026-05-10 14:19:35 +0800

    add sth

--- c/main.cpp
+++ c/main.cpp
@@ -11,5 +11,6 @@ int main(int argc, const char *argv[]) {
     // insert code here...
     std::cout << "zzg!!!" << std::endl;
     std::cout << "Hello, World!\n";
+    
     return EXIT_SUCCESS;
 }
