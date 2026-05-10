e241b5f34580779b015347d3f0719387ff635cde
zzg <2978644367@qq.com>
2026-05-10 13:25:25 +0800

    Initial Commit

---
+++ c/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  c
+//
+//  Created by 张志刚 on 2026/5/10.
+//
+
+#include <iostream>
+
+int main(int argc, const char *argv[]) {
+    // insert code here...
+    std::cout << "zzg!!!" << std::endl;
+    std::cout << "Hello, World!\n";
+    return EXIT_SUCCESS;
+}
